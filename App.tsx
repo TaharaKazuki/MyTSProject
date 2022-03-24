fee66973023d51cd26a48c07e3ff018d7c95c841
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-03-24 18:31:23 +0900

    update

--- App.tsx
+++ App.tsx
@@ -5,17 +5,41 @@
 // import useColorScheme from './hooks/useColorScheme'
 // import Navigation from './navigation'
 
+// export default function App() {
+// const isLoadingComplete = useCachedResources()
+// const colorScheme = useColorScheme()
+// if (!isLoadingComplete) {
+//   return null
+// } else {
+// return (
+//   <SafeAreaProvider>
+//     <Navigation colorScheme={colorScheme} />
+//     <StatusBar />
+//   </SafeAreaProvider>
+// )
+//   return <div>Hello</div>
+// }
+import React, { useState } from 'react'
+import {
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from 'react-native'
+import { colors } from './utils/colors'
+
 export default function App() {
-  // const isLoadingComplete = useCachedResources()
-  // const colorScheme = useColorScheme()
-  // if (!isLoadingComplete) {
-  //   return null
-  // } else {
-  // return (
-  //   <SafeAreaProvider>
-  //     <Navigation colorScheme={colorScheme} />
-  //     <StatusBar />
-  //   </SafeAreaProvider>
-  // )
-  return <div>Hello</div>
+  const [currentSubject, setCurrentSubject] = useState<string>('test')
+
+  return <SafeAreaView style={styles.container}>{!cu}</SafeAreaView>
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: colors.darkBlue,
+  },
+})
